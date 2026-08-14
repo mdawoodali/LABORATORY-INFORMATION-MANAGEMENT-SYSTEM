36e782ff4197482c07ef075d853ab1d4fd0eb7e5
S.R. Labs User <user@example.com>
2026-08-14 21:32:36 +0500

    Hardcode Supabase variables to fix Vercel deploy

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '';
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || "https://mrskydxtqadibddtvkrk.supabase.co";
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1yc2t5ZHh0cWFkaWJkZHR2a3JrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODY3MTg1ODUsImV4cCI6MjEwMjI5NDU4NX0.tHoRJg1FOYFDUNA0rPivEhhD2dXAKqyql_e16TQkG70";
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(supabaseUrl, supabaseKey);
