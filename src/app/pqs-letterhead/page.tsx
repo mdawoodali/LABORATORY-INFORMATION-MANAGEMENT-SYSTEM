f38e44dca15e5066ad97d8f0d45c46d9c7683e32
Areeb Iqbal <iqbalareeb26@gmail.com>
2026-09-13 22:22:15 +0500

    Reposition Date to top-right to completely eliminate overlapping, and drastically reduce empty space around Logo stack

--- src/app/pqs-letterhead/page.tsx
+++ src/app/pqs-letterhead/page.tsx
@@ -205,17 +205,17 @@ export default function PQSLetterheadPage() {
               
               {/* Header */}
               <div 
-                className="relative border-b-2 border-gray-800 pb-2 px-10 pt-2 z-10 flex flex-col items-center" 
+                className="relative border-b-2 border-gray-800 pb-2 px-10 z-10 flex flex-col items-center" 
               >
                 {/* Centered Logo Stack */}
-                <div className="flex flex-col items-center gap-1 mx-auto w-full -mt-2">
+                <div className="flex flex-col items-center mx-auto w-full -mt-6">
                   <PQSLogoImage style={{ width: '90px', height: '90px', objectFit: 'contain' }} />
-                  <PQSWordmark style={{ height: '26px', width: '252px', objectFit: 'contain' }} />
-                  <div className="text-xs text-gray-500 leading-tight whitespace-nowrap -mt-1" contentEditable suppressContentEditableWarning>Providing Consultancy Service to Textile Industries</div>
+                  <PQSWordmark style={{ height: '26px', width: '252px', objectFit: 'contain', marginTop: '-8px' }} />
+                  <div className="text-xs text-gray-500 leading-tight whitespace-nowrap mt-1" contentEditable suppressContentEditableWarning>Providing Consultancy Service to Textile Industries</div>
                 </div>
 
                 {/* Date field, absolute right aligned */}
-                <div className="absolute right-10 bottom-2 flex gap-2 text-sm justify-end items-end w-48">
+                <div className="absolute right-10 top-4 flex gap-2 text-sm justify-end items-end w-48">
                   <span className="font-bold whitespace-nowrap">Date:</span>
                   <input 
                     type="text" 
