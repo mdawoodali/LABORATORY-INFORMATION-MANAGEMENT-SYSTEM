f8031497669f76e8ceb3400b3796ac0b6e67bc10
Areeb Iqbal <iqbalareeb26@gmail.com>
2026-09-13 22:21:00 +0500

    Increase padding at the bottom of the letterhead footer for better breathing room

--- src/app/pqs-letterhead/page.tsx
+++ src/app/pqs-letterhead/page.tsx
@@ -236,8 +236,8 @@ export default function PQSLetterheadPage() {
             </div>
 
             {/* Footer */}
-            <div className="absolute bottom-0 left-0 w-full flex flex-col items-center z-10 bg-white pb-2">
-              <div className="w-[90%] flex justify-between border-t border-gray-400 pt-1 text-[11px] text-gray-500">
+            <div className="absolute bottom-0 left-0 w-full flex flex-col items-center z-10 bg-white pb-6">
+              <div className="w-[90%] flex justify-between border-t border-gray-400 pt-2 text-[11px] text-gray-500">
                 <span contentEditable suppressContentEditableWarning>R-332/9, Dastagir, F.B Area, Karachi, 75950.</span>
                 <span contentEditable suppressContentEditableWarning>03322673373 | 03333769174</span>
                 <span contentEditable suppressContentEditableWarning>precisionqualityserviceslabs@gmail.com</span>
