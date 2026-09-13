73f21d1842515c68b0b16f3f82adce650e862ad6
Areeb Iqbal <iqbalareeb26@gmail.com>
2026-09-13 22:59:10 +0500

    Fix report test table pagination rendering

--- src/app/editor/page.tsx
+++ src/app/editor/page.tsx
@@ -486,24 +486,33 @@ function EditorContent() {
           </div>
         </div>
 
-        {/* PAGE 2 */}
-        <div className="a4-page relative overflow-hidden flex flex-col bg-white">
-          <img src="/frame.png" alt="Frame" className="absolute top-0 left-0 w-full h-full z-0 pointer-events-none object-fill" style={{ imageRendering: '-webkit-optimize-contrast', filter: 'contrast(1.02)' }} />
-          <div className="relative z-10 w-full h-full flex flex-col">
-            <SubHeader reportNo={formData.reportNo} pageNum={2} totalPages={totalPages} />
-            <div className="pt-[175px] flex-1 flex flex-col">
-              <TestTable tests={tests} data={formData} />
-                
-                <div className="flex-1"></div>
-              <div className="pb-[55px] relative">
-                <Signature companyName={brandSettings.companyName} />
-                <div className="absolute bottom-2 left-0 w-full text-center text-[8px] text-gray-400 font-sans tracking-wide">
-                  This document was generated digitally and doesn&apos;t require a signature
+        {/* TEST PAGES (dynamically paginated) */}
+        {testChunks.map((chunk, chunkIdx) => (
+          <div key={`test-page-${chunkIdx}`} className="a4-page relative overflow-hidden flex flex-col bg-white">
+            <img src="/frame.png" alt="Frame" className="absolute top-0 left-0 w-full h-full z-0 pointer-events-none object-fill" style={{ imageRendering: '-webkit-optimize-contrast', filter: 'contrast(1.02)' }} />
+            <div className="relative z-10 w-full h-full flex flex-col">
+              <SubHeader reportNo={formData.reportNo} pageNum={2 + chunkIdx} totalPages={totalPages} />
+              <div className="pt-[175px] flex-1 flex flex-col">
+                <TestTable 
+                  tests={chunk} 
+                  data={formData} 
+                  isFirstPage={chunkIdx === 0}
+                  isLastPage={chunkIdx === testChunks.length - 1}
+                />
+                  
+                  <div className="flex-1"></div>
+                <div className="pb-[55px] relative">
+                  {chunkIdx === testChunks.length - 1 && (
+                    <Signature companyName={brandSettings.companyName} />
+                  )}
+                  <div className="absolute bottom-2 left-0 w-full text-center text-[8px] text-gray-400 font-sans tracking-wide">
+                    This document was generated digitally and doesn&apos;t require a signature
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
+        ))}
 
         {/* PAGE 3 - Sample Image */}
         {sampleImages.length > 0 && (
