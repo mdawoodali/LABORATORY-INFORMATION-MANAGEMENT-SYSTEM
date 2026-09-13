4e67364d6db77ae0d8e210f2f56f10955868a2c3
Areeb Iqbal <iqbalareeb26@gmail.com>
2026-09-13 21:12:07 +0500

    Remove temp files

--- make_ts.py
+++
@@ -1,5 +0,0 @@
-import os
-b64 = open('stamp_b64.txt', 'r').read().strip()
-out = f"export const PQSStampBase64 = 'data:image/png;base64,{b64}';\n"
-with open(r'src\components\report\PQSStampBase64.ts', 'w') as f:
-    f.write(out)
